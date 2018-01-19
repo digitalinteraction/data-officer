515d2eb46e8ade6b75914aefa676aa7541060ea6
Rob Anderson <robster31@googlemail.com>
2018-01-19 22:23:36 +0000

    Added docs & implemented dokku

--- web/index.js
+++ web/index.js
@@ -6,21 +6,10 @@ const pingers = require('./pingers')
 ;(async () => {
   
   let app = express()
+    .use(api.middleware())
   
-  app.use(api.middleware())
-  
-  
-  // Add each of the pingers
-  Object.keys(pingers).forEach(name => {
-    app.get(`/${name}`, async (req, res) => {
-      let pong = await pingers[name]()
-      res.api.sendData(pong)
-    })
-  })
-  
-  
-  // Add a general endpoint
-  app.get('/', async (req, res) => {
+  // Add an endpoint for all infos
+  app.get('/all', async (req, res) => {
     let pongs = {}
     await Promise.all(Object.keys(pingers).map(async name => {
       pongs[name] = await pingers[name]()
@@ -28,9 +17,22 @@ const pingers = require('./pingers')
     res.api.sendData(pongs)
   })
   
+  // Add an endpoint for each pingers
+  Object.keys(pingers).forEach(name => {
+    app.get(`/service/${name}`, async (req, res) => {
+      let pong = await pingers[name]()
+      res.api.sendData(pong, pong.state.online ? 200 : 400)
+    })
+  })
+  
+  // Add a state endpoint
+  app.get('/', (req, res) => {
+    res.api.sendData('ok')
+  })
+  
   
   // Listen on 3000
   await new Promise(resolve => app.listen(3000, resolve))
-  console.log('Listening on :8080')
+  console.log('Listening on :3000')
   
 })()
--- web/pingers.js
+++ web/pingers.js
@@ -3,58 +3,46 @@ const mysql = require('mysql')
 
 function httpPinger(url, info) {
   return async () => {
+    let status = null
+    try {
+      let res = await axios.get(url)
+      status = res.status
+    }
+    catch (error) {
+      status = (error.response && error.response.status) || 400
+    }
     
-    let res = await axios.get(url)
-    
-    return Object.assign(info, {
-      online: res.status === 200
-    })
+    return makeState(info, status)
   }
 }
 
-// function mysqlPinger(url, info) {
-//
-//   console.log(url)
-//
-//   let connection = mysql.createConnection(url)
-//   //
-//   // console.log(connection)
-//
-//   return () => { /* ... */ }
-//
-//   return async () => {
-//     connection.ping(error => {
-//       return Object.assign(info, { online: !error })
-//     })
-//   }
-// }
+function makeState(info, status, messages = []) {
+  return Object.assign({}, info, {
+    state: {
+      online: status === 200,
+      status,
+      messages: Array.isArray(messages) ? messages : messages
+    }
+  })
+}
 
-// exports.mysql_main = async function() {
-//
-//   return {
-//     name: 'Mysql ~ Main',
-//     info: 'MySQL server for our main website & Digital Civics',
-//     link: null,
-//     online: false
-//   }
-// }
+function mysqlPinger(uri, info) {
+  
+  let connection = mysql.createConnection(uri)
+  
+  return async () => {
+    
+    let error = await new Promise(resolve => connection.ping(resolve))
+    return makeState(info, error ? 400 : 200)
+  }
+}
 
-// exports.mysql_shared = async function() {
-//
-//   return {
-//     name: 'Mysql ~ Shared',
-//     info: 'MySQL for internal projects',
-//     link: null,
-//     online: false
-//   }
-// }
+exports.mysql_main = mysqlPinger('mysql://user:password@dig-mysql:3307/*', {
+  name: 'Mysql ~ Main', info: 'MySQL for our websites', link: null
+})
 
-// exports.mysql_main = mysqlPinger('mysql://dig-mysql:3306', {
-//   name: 'Mysql ~ Main', info: 'MySQL server for our main website & Digital Civics', link: null
-// })
-
-exports.dokku = httpPinger('http://dig-civics:1090/nginx', {
-  name: 'Dokku', info: 'Application Server', link: null
+exports.mysql_shared = mysqlPinger('mysql://user:password@dig-mysql:3306/*', {
+  name: 'Mysql ~ Shared', info: 'MySQL for internal projects', link: null
 })
 
 exports.openlab = httpPinger('https://openlab.ncl.ac.uk', {
@@ -76,3 +64,26 @@ exports.owncloud = httpPinger('https://openlab.ncl.ac.uk/owncloud', {
 exports.svn = httpPinger('https://openlab.ncl.ac.uk/csvn', {
   name: 'Subversion', info: 'Svn projects repository', link: null
 })
+
+exports.dokku = async () => {
+  
+  let dokku, gateway
+  
+  try {
+    [dokku, gateway] = await Promise.all([
+      axios.get('dig-civics:1090/nginx'),
+      axios.get('dig-gateway:27123')
+    ])
+  }
+  catch (error) { }
+  
+  let messages = []
+  if (!dokku) messages.push('Cannot connect to dig-civics generator endpoint (:1090)')
+  if (!gateway) messages.push('Cannot connect to dig-gateway reload endpoint (:27123)')
+  
+  let status = dokku && gateway ? 200 : 400
+  
+  return makeState({
+    name: 'Dokku', info: 'Shared Application Server', link: null
+  }, status, messages)
+}
