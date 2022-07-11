6f86f28533267438a7f7a71686e1349bfe5e08c7
Rob Anderson <robanderson@hey.com>
2022-07-11 09:53:22 +0100

    update version script to commit and tag with git

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read=app.json,CHANGELOG.md --allow-write=app.json
+#!/usr/bin/env -S deno run --allow-read=app.json,CHANGELOG.md --allow-write=app.json --allow-run=git
 
 const [newVersion] = Deno.args;
 
@@ -19,3 +19,18 @@ const newApp = app.replace(
 );
 
 Deno.writeTextFile("app.json", newApp);
+
+const gitAdd = await Deno.run({
+  cmd: ["git", "add", "app.json", "CHANGELOG.md"],
+}).status();
+if (gitAdd.code !== 0) throw new Error("Failed to stage code");
+
+const gitCommit = await Deno.run({
+  cmd: ["git", "commit", "-m", newVersion],
+}).status();
+if (gitCommit.code !== 0) throw new Error("Failed to commit");
+
+const gitTag = await Deno.run({
+  cmd: ["git", "tag", "v" + newVersion, "-m", "v" + newVersion],
+}).status();
+if (gitTag.code !== 0) throw new Error("Failed to tag");
