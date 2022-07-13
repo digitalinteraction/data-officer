5c86a8ea465a84434cf9e9146011598157517f35
Rob Anderson <robanderson@hey.com>
2022-07-13 23:42:35 +0100

    add coffee-club repo for current day's consumption

--- src/repos/coffee_club.ts
+++ src/repos/coffee_club.ts
@@ -1,7 +1,14 @@
-import { expandGlob, parseCsv } from "../../deps.ts";
+import { expandGlob, parseCsv, path } from "../../deps.ts";
 import { GitRepository } from "../lib/mod.ts";
 
 export const COFFEE_CLUB_REPO_BASE = "repos/coffee-club";
+
+export interface ConsumptionJson {
+  date: string;
+  user: string;
+  resource: string;
+  quantity: number;
+}
 
 export async function getLatestConsumption(since: Date) {
   const files = expandGlob(`${COFFEE_CLUB_REPO_BASE}/users/*/use-*.csv`);
@@ -51,6 +58,43 @@ export function getCoffeeClubRepo(): GitRepository {
     return parseCsv(file, {
       columns: ["id", "resource", "quantity", "creator"],
     });
+  };
+
+  // Assumes rows are sorted oldest-first
+  repo.collections.today = async () => {
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const files = expandGlob(`${COFFEE_CLUB_REPO_BASE}/users/*/use-*.csv`);
+    const output: ConsumptionJson[] = [];
+    for await (const file of files) {
+      if (!file.isFile) continue;
+
+      const [user] = path.parse(file.path).dir.split(path.sep).slice(-1);
+
+      const data = await parseCsv(await Deno.readTextFile(file.path), {
+        columns: ["date", "resource", "quantity"],
+      });
+
+      for (const row of data.reverse()) {
+        const date = new Date(row.date as string);
+        const quantity = parseInt(row.quantity as string);
+
+        if (Number.isNaN(date.getTime()) || Number.isNaN(quantity)) {
+          continue;
+        }
+        if (date.getTime() < startOfDay.getTime()) break;
+
+        output.push({
+          date: row.date as string,
+          quantity,
+          resource: row.resource as string,
+          user: user ?? "unknown",
+        });
+      }
+    }
+    output.sort((a, b) => a.date.localeCompare(b.date));
+    return output;
   };
 
   return repo;
