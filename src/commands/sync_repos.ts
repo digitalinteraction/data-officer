50652850249f91ac09ccbc56bee9405af58a0bea
Rob Anderson <robanderson@hey.com>
2022-12-13 17:02:56 +0000

    wrap sync_repos with a semaphore

--- src/commands/sync_repos.ts
+++ src/commands/sync_repos.ts
@@ -13,6 +13,9 @@ options:
   --help  Show this help message
 `;
 
+const SEMAPHORE_KEY = "cmd/sync_repos";
+const SEMAPHORE_DURATION = 2 * 60;
+
 export const syncReposCommand: Command = {
   name: "sync_repos",
   info: "Run the repo sync script and load collections into redis",
@@ -26,7 +29,18 @@ export const syncReposCommand: Command = {
 
     const redis = await redisClientFromEnv(env);
 
+    // https://redis.io/commands/setnx/
+    const isLocked = await redis.setnx(SEMAPHORE_KEY, "1");
+    if (isLocked !== 1) {
+      console.error("Sync already running");
+      Deno.exit(1);
+    }
+    await redis.expire(SEMAPHORE_KEY, SEMAPHORE_DURATION);
+
     const success = await syncRepos(redis, getAllRepos());
+
+    await redis.del(SEMAPHORE_KEY);
+
     if (!success) Deno.exit(1);
   },
 };
